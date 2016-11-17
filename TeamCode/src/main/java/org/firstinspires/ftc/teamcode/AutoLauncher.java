8948267856cad6c96f95e1bb36240f4f9f543b24
FTC Team 8424 <crms.robotics@gginbox.net>
2016-11-16 18:27:05 -0600

    Made launch servo drop one second after being pressed. Full power on launcher = 1613 encoder ticks, press gamepad2.b to decrease power by 100 encoder ticks.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLauncher.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 /**
  * Created by FTC8424 on 11/16/2016.
  */
 
-public class AutoLauncher {
+public class AutoLauncher extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
